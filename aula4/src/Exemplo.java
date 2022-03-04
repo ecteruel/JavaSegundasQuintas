67010c2981ae66d4694cca76a59a57225ba92701
ecteruel <ecteruel@gmail.com>
2022-03-03 22:05:18 -0300

    Atualização dia 03/03/2022

--- aula4/src/Exemplo.java
+++ aula4/src/Exemplo.java
@@ -1,9 +1,14 @@
-
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-public class Exemplo extends JFrame {
 
+public class Exemplo extends JFrame {
+    //Atributos
+    private JLabel lblNome, lblEndereco; 
+    private JTextField txtNome, txtEndereco;
+    private JButton btnSalvar;    
+       
     //Construtor - gerar os elementos de tela. 
     //Deve ter o mesmo nome da classe
     public Exemplo() {
@@ -15,23 +20,39 @@ public class Exemplo extends JFrame {
         setLocation(200,300); // 200px margem esquerda e 300px acima
 
        //Comando para colocar elementos na janela
-       JLabel lblNome; //Declarando a variável do rótulo
        lblNome = new JLabel("Nome"); // Determina o que aparecerá escrito no rótulo
        lblNome.setSize(80,30); //Determina a largura e altura do rótulo
        lblNome.setLocation(10,20); //Determina margem esquerda e superior
        add(lblNome); // Adiciona o rótulo na janela JFrame
        
-       JTextField txtNome;
        txtNome = new JTextField();
        txtNome.setSize(400,30);
-       txtNome.setLocation(50, 20);
-       add(txtNome);               
+       txtNome.setLocation(90, 20);
+       add(txtNome);         
+       
+       lblEndereco = new JLabel("Endereço:");
+       lblEndereco.setSize(100,30);
+       lblEndereco.setLocation(10,60);
+       add(lblEndereco);
+       
+       txtEndereco = new JTextField();
+       txtEndereco.setSize(500,30);
+       txtEndereco.setLocation(90,60);
+       add(txtEndereco);
+
+       btnSalvar = new JButton("Salvar");
+       btnSalvar.setSize(80,30);
+       btnSalvar.setLocation(10,100);
+       add(btnSalvar);
        
     }    
     
+    //Métodos
     public static void main(String[] args) {
-        Exemplo ex;
-        ex = new Exemplo();
-        ex.setVisible(true);
+        Exemplo tela;
+        tela = new Exemplo();
+        tela.setVisible(true);
     }
+
+
 }
